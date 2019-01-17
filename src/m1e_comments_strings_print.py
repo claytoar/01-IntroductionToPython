d76eb7a2762cc1ea790cfc73be0394c9120c13c4
claytoar <claytoar@rose-hulman.edu>
2019-01-17 09:38:08 -0500

    working

--- src/m1e_comments_strings_print.py
+++ src/m1e_comments_strings_print.py
@@ -22,7 +22,7 @@
 ###############################################################################
 
 
-print('Hello, World')
+print('Hello, Abi')
 print('hi there')
 print('one', 'two', 'buckle my shoe')
 
